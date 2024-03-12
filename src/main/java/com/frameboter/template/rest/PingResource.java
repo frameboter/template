f2576a579da7b642753a26a3ba43abdd9dde00ab
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2024-03-12 19:29:06 +0100

    Add files via upload

---
+++ src/main/java/com/frameboter/template/rest/PingResource.java
@@ -0,0 +1,23 @@
+package com.frameboter.template.rest;
+import com.frameboter.rest.AbstractResource;
+import jakarta.annotation.security.RolesAllowed;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class PingResource extends AbstractResource {
+
+    @PostMapping("/ping")
+    @RolesAllowed("TEST")
+    void createPing(@AuthenticationPrincipal Jwt jwt) {
+
+        List<String> roles = (List<String>) jwt.getClaimAsMap("realm_access").getOrDefault("roles", new ArrayList());
+        String role = roles.get(0);
+    }
+}
