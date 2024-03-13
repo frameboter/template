fa4edff08f05224ed81cb6497ffaa8c30fdc573b
roboter5123 <pascal.roboter@hotmail.com>
2024-03-13 17:57:41 +0100

    Added a ping resource

--- src/main/java/com/frameboter/template/rest/PingResource.java
+++ src/main/java/com/frameboter/template/rest/PingResource.java
@@ -1,21 +1,20 @@
 package com.frameboter.template.rest;
+
 import com.frameboter.rest.AbstractResource;
+import com.frameboter.template.rest.model.Ping;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.time.LocalDateTime;
 
 @RestController
 public class PingResource extends AbstractResource {
 
     @PostMapping("/ping")
-    void createPing(@AuthenticationPrincipal Jwt jwt) {
+    Ping createPing(@AuthenticationPrincipal Jwt jwt) {
 
-        List<String> roles = (List<String>) jwt.getClaimAsMap("realm_access").getOrDefault("roles", new ArrayList());
-        String role = roles.get(0);
+        return new Ping(getUserName(jwt), LocalDateTime.now());
     }
 }
