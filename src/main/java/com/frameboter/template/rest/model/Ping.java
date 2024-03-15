dfd8c8cbb5e65ab457cf7c8da30182b65c7df79e
roboter5123 <pascal.roboter@hotmail.com>
2024-03-15 17:50:56 +0100

    Formatting

--- src/main/java/com/frameboter/template/rest/model/Ping.java
+++ src/main/java/com/frameboter/template/rest/model/Ping.java
@@ -13,6 +13,6 @@ public class Ping {
     @Schema(name = "username", example = "roboter5123", description = "Name of the currently logged in user")
     private String userName;
 
-    @Schema(name = "createdAt", example = "1999-06-13T10:00:55.5517585", description = "Date and time at which the ping was created", pattern = "YYYY-MM-DDThh:mm:ss.ms")
+    @Schema(name = "createdAt", example = "1999-06-13T10:00:00.0000000", description = "Date and time at which the ping was created", pattern = "YYYY-MM-DDThh:mm:ss.ms")
     private LocalDateTime createdAt;
 }
