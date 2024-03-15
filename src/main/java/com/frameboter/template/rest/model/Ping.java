bb3c5e8b19c9c586f2c900637a88ccfbce55dc9e
roboter5123 <pascal.roboter@hotmail.com>
2024-03-15 17:37:39 +0100

    Added Swagger Annotations

--- src/main/java/com/frameboter/template/rest/model/Ping.java
+++ src/main/java/com/frameboter/template/rest/model/Ping.java
@@ -1,5 +1,6 @@
 package com.frameboter.template.rest.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,7 +10,9 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class Ping {
 
+    @Schema(name = "username", example = "roboter5123", description = "Name of the currently logged in user")
     private String userName;
 
-    private LocalDateTime time;
+    @Schema(name = "createdAt", example = "1999-06-13T10:00:55.5517585", description = "Date and time at which the ping was created", pattern = "YYYY-MM-DDThh:mm:ss.ms")
+    private LocalDateTime createdAt;
 }
